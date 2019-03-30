001fb409aeb0b8d06cb27c4358345a5a050cddf4
Alexander <Philalex196401@gmail.com>
2019-03-31 02:42:13 +0300

    VKLite - problem file

---
+++ iOSLessonOne/VK_Lite_1/VK_Lite_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK_Lite_1
+//
+//  Created by Александр Филиппов on 31.03.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOSLessonOne/iOSLessonOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLessonOne
+//
+//  Created by Александр Филиппов on 30.03.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
