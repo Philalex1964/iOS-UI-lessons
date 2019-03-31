ed80d56b1bfb79c46f23a82bb9510b2e9934a3e0
Alexander <Philalex196401@gmail.com>
2019-04-01 01:01:25 +0300

    Lesson_1 - 3rd attempt - TC - 1:52:44
    ok by now

---
+++ iOSLessonOne/VK_Lite_1/VK_Lite_1/LoginScreenController.swift
@@ -0,0 +1,55 @@
+//
+//  LoginScreenController.swift
+//  VK_Lite_1
+//
+//  Created by Александр Филиппов on 31.03.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreenController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print(#function)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print(#function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print(#function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print(#function)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        print(#function)
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        print(#function)
+    }
+
+}
