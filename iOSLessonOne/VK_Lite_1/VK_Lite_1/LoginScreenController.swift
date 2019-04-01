2ddc55c6cc89605b3578726e696e92ef662531c6
Alexander <Philalex196401@gmail.com>
2019-04-02 01:49:42 +0300

    The end of the lesson 1.
    Все работает кроме, при этом объявляется конфликт длинн , который автоматически удаляется но все работает. И горизонтальный скролл.

--- iOSLessonOne/VK_Lite_1/VK_Lite_1/LoginScreenController.swift
+++ iOSLessonOne/VK_Lite_1/VK_Lite_1/LoginScreenController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class LoginScreenController: UIViewController {
-
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print(#function)
     }
     
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        print(#function)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasHidden(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -31,7 +37,8 @@ class LoginScreenController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        print(#function)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -40,7 +47,7 @@ class LoginScreenController: UIViewController {
         print(#function)
     }
     
-    override func viewWillLayoutSubviews() {
+    /*override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
         print(#function)
@@ -50,6 +57,35 @@ class LoginScreenController: UIViewController {
         super.viewWillTransition(to: size, with: coordinator)
         
         print(#function)
-    }
+    }*/
 
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if usernameInput.text == "admin",
+            passwordInput.text == "123456" {
+            print("Authorization successful")
+        } else {
+            print("Authorization failed")
+
+        }
+        
+    }
+    
+    @objc func keyboardWasShown(notification: Notification) {
+        let info = notification.userInfo as NSDictionary?
+        let keyboardSize = (info?.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWasHidden(notification: Notification) {
+        let contentInsets = UIEdgeInsets.zero
+        
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        
+    }
+    
 }
