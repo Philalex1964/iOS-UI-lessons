9a7d4c40273263107cad57244ff6ab64e84c89c5
Alexander <Philalex196401@gmail.com>
2019-04-07 11:40:48 +0300

    Homework-2

---
+++ iOSLessonOne/VK_Lite_1/VK_Lite_1/MainTabbarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabbarController.swift
+//  VK_Lite_1
+//
+//  Created by Александр Филиппов on 07.04.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import UIKit
+
+class MainTabbarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
