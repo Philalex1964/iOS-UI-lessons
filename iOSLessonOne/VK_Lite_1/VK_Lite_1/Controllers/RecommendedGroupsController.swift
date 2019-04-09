d8609acf0339d364cdeef6997297e234cf5a532b
Alexander <Philalex196401@gmail.com>
2019-04-09 19:21:08 +0300

    Connected TableView to RecGroupsController

---
+++ iOSLessonOne/VK_Lite_1/VK_Lite_1/Controllers/RecommendedGroupsController.swift
@@ -0,0 +1,30 @@
+//
+//  RecommendedGroupsController.swift
+//  VK_Lite_1
+//
+//  Created by Александр Филиппов on 09.04.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import UIKit
+
+class RecommendedGroupsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
