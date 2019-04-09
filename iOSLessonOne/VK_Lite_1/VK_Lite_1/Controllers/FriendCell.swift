33122fa6e883a98f7fdf42de721f69fb06c4635c
Alexander <Philalex196401@gmail.com>
2019-04-09 17:16:49 +0300

    connected friendnameLabel to FriendCell

---
+++ iOSLessonOne/VK_Lite_1/VK_Lite_1/Controllers/FriendCell.swift
@@ -0,0 +1,18 @@
+//
+//  FriendCell.swift
+//  VK_Lite_1
+//
+//  Created by Александр Филиппов on 09.04.2019.
+//  Copyright © 2019 Philalex. All rights reserved.
+//
+
+import UIKit
+
+class FriendCell: UITableViewCell {
+
+    @IBOutlet weak var friendnameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
