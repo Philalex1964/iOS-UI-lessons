bce82a9ec19ac5f95e0baccb61d252a508306f8d
Alexander <Philalex196401@gmail.com>
2019-04-25 18:41:02 +0300

    Lesson6.8
    подключил аутлеты

--- iOSLessonOne/VK_Lite_1/VK_Lite_1/Controllers/Cells/NewsCell.swift
+++ iOSLessonOne/VK_Lite_1/VK_Lite_1/Controllers/Cells/NewsCell.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class NewsCell: UITableViewCell {
+    
+    static let reuseId = "NewsCell"
 
+    @IBOutlet weak var newsTextViwq: UITextView!
+    @IBOutlet weak var newsImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var commentsButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var sharesLabel: UILabel!
+    @IBOutlet weak var commentsLabel: UILabel!
+    @IBOutlet weak var eyeImage: UIImageView!
+    @IBOutlet weak var seenLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
